9854c8561903ce734ea10c8ab597f81522629f5f
han <wuuuuhan@outlook.com>
2022-03-28 18:01:01 +0800

    调整目录库结构

--- routes/upload.js
+++ routes/upload.js
@@ -1,40 +1,32 @@
-var express = require('express');
+//文件上传路由
+const express = require('express');
+const router = express.Router();
 const exec = require('child_process').exec;
 const fs = require('fs');
 const pathLib = require('path');
-var app = express();
+
+
 // 引入body-parser中间件，用来处理post请求体body中的数据
 const bodyParser = require('body-parser')
     // 引入multer中间件，用于处理上传的文件数据
 const multer = require('multer')
 
+var app = express();
 
-const server = app.listen(3000, function() {
-    console.log("http://localhost:3000");
-    // console.log('express + multer 实现文件上传')
-})
-
-app.get('/', function(res, rep) {
-    // rep.send('Hello, word!6666666');
-    rep.sendFile(__dirname + '/src/index.html')
-
-});
-
-// 读取静态资源
-app.use(express.static('public'))
-    // 通过配置multer的dest属性， 将文件储存在项目下的tmp文件中
+// 通过配置multer的dest属性， 将文件储存在项目下的tmp文件中
 app.use(multer({
     dest: 'tmp/'
 }).any())
 
-// 访问index.html页面
-app.get('index.html', function(req, res) {
-    // 将public下的index.html文件返回去
-    res.sendFile(__dirname + '/src/index.html')
-})
+router.get('/', function(res, rep) {
+    rep.send('文件上传接口');
+    // rep.sendFile(__dirname + 'index4.ejs')
+
+});
+
 
 // 文件上传接口
-app.post('/fileUpload', function(req, res) {
+router.post('/', function(req, res) {
     for (let i = 0; i < req.files.length; i++) {
         console.log(req.files[i])
         let filename = req.files[i].destination + new Date().getTime() + pathLib.parse(req.files[i].originalname).ext;
@@ -73,3 +65,5 @@ app.post('/fileUpload', function(req, res) {
      }) */
 
 })
+
+module.exports = router;
